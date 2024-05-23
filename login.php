bfc23850e2678d90a9565b40a5e544c996d6df01
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-23 18:09:55 +0800

    update login

--- login.php
+++ login.php
@@ -27,32 +27,28 @@
 </head>
 <body>
 
-<div class="header">
-  <a href="#default" class="logo">CompanyLogo</a>
-  <div class="header-right">
-    <a class="active" href="#home">Home</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-  </div>
-</div>
-
 <!-- Page Content -->
 <div class="container">
     <!-- <div class="row"> -->
         <div class="box">
             <!--<h1 class='text-white'>LOGIN</h1>-->
               <div class="form-login"></br>
+              <div class="logo">
+              <a>
+                <img src="img/logo.png" alt="Vendor" width="180" height="100" position="center">
+              </a>
+              </div>
                 <h4>LOGIN</h4>
                 </br>
                 <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="username"/>
                 </br></br>
                 <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="password"/>
                 </br></br>
-                <select id="userType" class="form-control">
+                <!--select id="userType" class="form-control">
                     <option value="Admin">Admin</option>
                     <option value="Unit Keselamatan Staff">Unit Keselamatan Staff</option>
                     <option value="Student">Student</option>
-                </select>
+                </select-->
                 <div class="wrapper">
                         <span class="group-btn">
                             <a href="#" class="btn btn-danger btn-md">login <i class="fa fa-sign-in"></i></a>
---
+++ login2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FKPark</title>
+</head>
+<body>
+    <form>
+        <label>Username</label>
+        <input type="text" id="username" name="username">
+
+        <label>Password</label>
+        <input type="text" id="password" name="password">
+    </form>
+
+</body>
+</html>
