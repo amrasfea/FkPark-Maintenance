d33bfe1d240c8b3a591ab7018538e792a16fd2f9
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-24 01:07:59 +0800

    summon

--- staff/summon.php
+++ staff/summon.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Summon</title>
     <!--EXTERNAL CSS-->
+    <link rel="stylesheet" href="../css/summon.css">
     <link rel="icon" type="image/x-icon" href="../img/logo.png">
     <!--FAVICON-->
 
