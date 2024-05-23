f58cb2e732668867ac677565df885a094fc2b184
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-24 02:28:36 +0800

    summon

---
+++ staff/violation.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Violation Guideline</title>
+    <!--FAVICON-->
+    <link rel="icon" type="image/x-icon" href="../img/logo.png">
+</head>
+<body>
+    <?php include('../navigation/staffNav.php'); ?>
+    <h2>Traffic Violation Guide and Demerit</h2>
+    <table>
+        <tr>
+            <th>Traffic Violation</th>
+            <th>Demerit Points</th>
+        </tr>
+        <tr>
+            <td>Parking violation</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>Not comply in campus traffic regulations</td>
+            <td>15</td>
+        </tr>
+        <tr>
+            <td>Accident caused</td>
+            <td>20</td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <th>Total point</th>
+            <th>Enforcement type</th>
+        </tr>
+        <tr>
+            <td>Less than 20 points</td>
+            <td>Warning given</td>
+        </tr>
+        <tr>
+            <td>Less than 50 points</td>
+            <td>Revoke of in campus vehicle permission for 1 semester</td>
+        </tr>
+        <tr>
+            <td>Less than 80 points</td>
+            <td>Revoke of in campus vehicle permision for 2 semesters</td>
+        </tr>
+        <tr>
+            <td>More than 80 points</td>
+            <td>Revoke of in campus vehicle permision for entire study duration</td>
+        </tr>
+    </table>
+</body>
+</html>
