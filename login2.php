0f28ea7ed5d822e1f86d762162d47ad3afc7a3ec
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-23 18:35:27 +0800

    login2

--- login2.php
+++ login2.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FKPark</title>
+  <title>Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--EXTERNAL CSS-->
+  <link rel="stylesheet" href="css/login2.css">
 </head>
+
 <body>
-    <form>
-        <label>Username</label>
-        <input type="text" id="username" name="username">
-
-        <label>Password</label>
-        <input type="text" id="password" name="password">
-    </form>
-
+    <div class="container">
+        <a href="index.php">
+            <img src="img/logo.png" alt="Logo" width="180" height="100">
+        </a>
+        <div class="form">
+            <h1 class="title"><i>FKPark Management System</i></h1>
+            <div class="content">
+                <div class="row">
+                    <p>Login to your Account</p>
+                </div>
+                <form action="functions/submitLogin.php" method="post" name="login" id="login">
+                    <div class="row">
+                        <div class="col mb-3">
+                            Your Username: <input type="text" name="username" placeholder="Username" required />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col mb-3">
+                            Your Password: <input type="password" name="password" placeholder="Password" required />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col mb-3">
+                            <input name="submit" type="submit" value="Login" />
+                        </div>
+                    </div>
+                </form>
+                
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
