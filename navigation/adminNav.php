7199ee2035ddf7d0a260888ee29d97ed743faf59
umaishu03 <164764434+umaishu03@users.noreply.github.com>
2024-05-24 13:26:25 +0800

    ..

--- navigation/adminNav.php
+++ navigation/adminNav.php
@@ -222,10 +222,10 @@
     </a>
     <ul id="publicationDropdown" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
         <li class="sidebar-item">
-            <a href="../admin/MODULE2/addPark.php" class="sidebar-link">New Parking Spaces</a>
+            <a href="../admin/addPark.php" class="sidebar-link">New Parking Spaces</a>
         </li>
         <li class="sidebar-item">
-            <a href="../admin/MODULE2/listPark.php" class="sidebar-link">List Parking Spaces</a>
+            <a href="../admin/listPark.php" class="sidebar-link">List Parking Spaces</a>
         </li>
     </ul>
 </li>
