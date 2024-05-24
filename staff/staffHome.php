28c270b1d1fba9d546e37e935d064a598af27f1d
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-24 13:30:46 +0800

    module 4

---
+++ staff/staffHome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <!--FAVICON-->
+    <link rel="icon" type="image/x-icon" href="../img/logo.png">
+</head>
+<body>
+<?php include('../navigation/staffNav.php'); ?>
+
+</body>
+</html>
