bac70ca1d1f7523e8dfc96767782564ce4cc6516
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-24 13:37:33 +0800

    staffnav

--- staff/staffHome.php
+++ staff/staffHome.php
@@ -6,9 +6,31 @@
     <title>Dashboard</title>
     <!--FAVICON-->
     <link rel="icon" type="image/x-icon" href="../img/logo.png">
+    <!--EXTERNAL CSS-->
+    <link rel="stylesheet" href="../css/staffDashboard.css">
 </head>
 <body>
 <?php include('../navigation/staffNav.php'); ?>
-
+    <div class="dashboard">
+        <h1>Dashboard</h1>
+        <div class="statistics">
+            <div class="statistic">
+                <h2>Total Users</h2>
+                <p class="statistic-value">1000</p>
+            </div>
+            <div class="statistic">
+                <h2>Active Users</h2>
+                <p class="statistic-value">800</p>
+            </div>
+            <div class="statistic">
+                <h2>Inactive Users</h2>
+                <p class="statistic-value">200</p>
+            </div>
+            <div class="statistic">
+                <h2>Total Revenue</h2>
+                <p class="statistic-value">$50,000</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
