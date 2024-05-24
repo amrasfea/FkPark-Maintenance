862ee26ab188d93aa51fe6859217a6340dd6d1fe
amrasfea <sofeaamira51@gmail.com>
2024-05-24 17:41:04 +0800

    sopea

--- staff/editstaffProfile.php
+++ staff/editstaffProfile.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="icon" type="image/x-icon" href="../img/logo.png">
     <style>
         .field-input {
             border: none;
