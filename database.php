9ee4dcc2796bbb3b077704c7ae70cc052f64a4ab
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-28 19:08:26 +0800

    data

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$server = "localhost";
+/*$server = "localhost";
 $username = "root";
 $password = "";
 $dbname = "fkpark";
@@ -22,4 +22,158 @@ if (mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS $dbname")) {
 // Select database
 mysqli_select_db($conn, $dbname) or die(mysqli_error($conn));
 
-?>
+// MODULE 1
+// TABLE: USER
+$createUserTableQuery = "CREATE TABLE IF NOT EXISTS user (
+    u_id INT AUTO_INCREMENT PRIMARY KEY,
+    u_email VARCHAR(100) NOT NULL UNIQUE,
+    u_password VARCHAR(255) NOT NULL,
+    u_type ENUM('Unit Keselamatan Staff','Administrators','Student') NOT NULL
+)";
+
+if (mysqli_query($conn, $createUserTableQuery)) {
+    echo "User table created successfully or already exists.<br>";
+} else {
+    die("Error creating user table: " . mysqli_error($conn));
+}
+
+
+// TABLE: PROFILE
+$createProfileTableQuery = "CREATE TABLE IF NOT EXISTS profiles (
+    p_id INT AUTO_INCREMENT PRIMARY KEY,
+    p_name VARCHAR(100) NOT NULL,
+    p_course ENUM('SOFTWARE ENGINEERING', 'NETWORKING', 'GRAPHIC DESIGN') DEFAULT NULL,
+    p_faculty VARCHAR(100) DEFAULT NULL,
+    p_icNumber VARCHAR(15) NOT NULL,
+    p_email VARCHAR(100) NOT NULL,
+    p_phoneNum VARCHAR(15) DEFAULT NULL,
+    p_address VARCHAR(100) DEFAULT NULL,
+    p_postCode VARCHAR(10) DEFAULT NULL,
+    p_country VARCHAR(50) DEFAULT NULL,
+    p_state VARCHAR(50) DEFAULT NULL,
+    p_department VARCHAR(100) DEFAULT NULL,
+    p_bodyNumber VARCHAR(10) DEFAULT NULL,
+    p_position VARCHAR(100) DEFAULT NULL,
+    u_id INT,
+    FOREIGN KEY (u_id) REFERENCES user (u_id)
+)";
+
+if (mysqli_query($conn, $createProfileTableQuery)) {
+    echo "Profile table created successfully or already exists.<br>";
+} else {
+    die("Error creating profile table: " . mysqli_error($conn));
+}
+
+// TABLE: VEHICLE
+$createVehicleTableQuery = "CREATE TABLE IF NOT EXISTS vehicle (
+    v_id INT AUTO_INCREMENT PRIMARY KEY,
+    v_vehicleType ENUM('MOTORCYCLE','CAR') NOT NULL,
+    v_brand VARCHAR(50) NOT NULL,
+    v_roadTaxValidDate DATE NOT NULL,
+    v_licenceValidDate DATE NOT NULL,
+    v_licenceClass VARCHAR(5) NOT NULL,
+    v_phoneNum INT NOT NULL,
+    v_vehicleGrant BLOB NOT NULL,
+    v_approvalStatus ENUM('Reject','Approve') DEFAULT NULL,
+    v_remarks TEXT DEFAULT NULL,
+    v_qrCode VARCHAR(500) DEFAULT NULL,
+    v_plateNum VARCHAR(10) NOT NULL,
+    v_model VARCHAR(50) NOT NULL,
+    u_id INT,
+    FOREIGN KEY (u_id) REFERENCES user (u_id)
+)";
+
+if (mysqli_query($conn, $createVehicleTableQuery)) {
+    echo "Vehicle table created successfully or already exists.<br>";
+} else {
+    die("Error creating vehicle table: " . mysqli_error($conn));
+}
+
+// MODULE 2
+// TABLE: PARKSPACE
+$createParkSpaceTableQuery = "CREATE TABLE IF NOT EXISTS parkSpace(
+    ps_id VARCHAR(10) PRIMARY KEY,
+    ps_area VARCHAR(10) NOT NULL,
+    ps_category VARCHAR(10) NOT NULL,
+    ps_date DATE NOT NULL,
+    ps_time TIME NOT NULL,
+    ps_typeEvent VARCHAR(50) DEFAULT NULL,
+    ps_descriptionEvent VARCHAR(50) DEFAULT NULL,
+    ps_QR VARCHAR(255) DEFAULT NULL,
+    ps_availableStat VARCHAR(10) DEFAULT NULL
+    )";
+
+if (mysqli_query($conn, $createParkSpaceTableQuery)) {
+    echo "ParkSpace table created successfully or already exists.<br>";
+} else {
+    die("Error creating ParkSpace table: " . mysqli_error($conn));
+}
+
+// MODULE 3
+// TABLE: BOOKINFO
+$createBookInfoTableQuery = "CREATE TABLE IF NOT EXISTS bookInfo(
+    b_id INT AUTO_INCREMENT PRIMARY KEY,
+    u_id INT,
+    b_date DATE NOT NULL,
+    b_time TIME NOT NULL,
+    b_parkStart TIME DEFAULT NULL,
+    b_duration INT DEFAULT NULL,
+    b_status VARCHAR(10)DEFAULT NULL,
+    b_QRid VARCHAR(255) DEFAULT NULL,
+    v_id INT ,
+    ps_id VARCHAR(10),
+    FOREIGN KEY (u_id) REFERENCES user(u_id)
+    FOREIGN KEY (v_id) REFERENCES vehicle(v_id),
+   FOREIGN KEY (ps_id) REFERENCES parkSpace(ps_id)
+)";
+
+if (mysqli_query($conn, $createBookInfoTableQuery)) {
+    echo "BookInfo table created successfully or already exists.<br>";
+} else {
+    die("Error creating BookInfo table: " . mysqli_error($conn));
+}
+
+// MODULE 4 
+
+
+//TABLE :VIOLATION TYPE
+$createViolationTypeTableQuery = "CREATE TABLE IF NOT EXISTS violationType (
+    vt_id INT AUTO_INCREMENT PRIMARY KEY,
+    vt_name VARCHAR(200) NOT NULL,
+    vt_demeritPoints INT NOT NULL
+)";
+
+if (mysqli_query($conn, $createViolationTypeTableQuery)) {
+    echo "ViolationType table created successfully or already exists.<br>";
+} else {
+    die("Error creating ViolationType table: " . mysqli_error($conn));
+}
+
+// TABLE: SUMMON
+$createSummonTableQuery = "CREATE TABLE IF NOT EXISTS summon (
+    sum_id INT AUTO_INCREMENT PRIMARY KEY,
+    sum_date DATE DEFAULT NULL,
+    sum_vModel VARCHAR(50) NOT NULL,
+    sum_vBrand VARCHAR(100) NOT NULL,
+    sum_vPlate VARCHAR(10) NOT NULL,
+    sum_location VARCHAR(10) NOT NULL,
+    sum_violationType VARCHAR(200) NOT NULL,
+    sum_demerit INT NOT NULL,
+    sum_status VARCHAR(200) DEFAULT NULL,
+    sum_QR VARCHAR(200) DEFAULT NULL,
+    vt_id INT DEFAULT NULL,
+    v_id INT NOT NULL,
+    FOREIGN KEY (vt_id) REFERENCES violationType (vt_id),
+    FOREIGN KEY (v_id) REFERENCES vehicle(v_id)
+    
+   
+)";
+
+if (mysqli_query($conn, $createSummonTableQuery)) {
+    echo "Summon table created successfully or already exists.<br>";
+} else {
+    die("Error creating Summon table: " . mysqli_error($conn));
+}
+
+mysqli_close($conn);
+?>*/
---
+++ fkpark.sql
@@ -0,0 +1,282 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2024 at 12:12 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fkpark`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookinfo`
+--
+
+CREATE TABLE `bookinfo` (
+  `b_id` int(11) NOT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  `b_date` date NOT NULL,
+  `b_time` time NOT NULL,
+  `b_parkStart` time DEFAULT NULL,
+  `b_duration` int(11) DEFAULT NULL,
+  `b_status` varchar(10) DEFAULT NULL,
+  `b_QRid` varchar(255) DEFAULT NULL,
+  `v_id` int(11) DEFAULT NULL,
+  `ps_id` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parkspace`
+--
+
+CREATE TABLE `parkspace` (
+  `ps_id` varchar(10) NOT NULL,
+  `ps_area` varchar(10) NOT NULL,
+  `ps_category` varchar(10) NOT NULL,
+  `ps_date` date NOT NULL,
+  `ps_time` time NOT NULL,
+  `ps_typeEvent` varchar(50) DEFAULT NULL,
+  `ps_descriptionEvent` varchar(50) DEFAULT NULL,
+  `ps_QR` varchar(255) DEFAULT NULL,
+  `ps_availableStat` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `p_id` int(11) NOT NULL,
+  `p_name` varchar(100) NOT NULL,
+  `p_course` enum('SOFTWARE ENGINEERING','NETWORKING','GRAPHIC DESIGN') DEFAULT NULL,
+  `p_faculty` varchar(100) DEFAULT NULL,
+  `p_icNumber` varchar(15) NOT NULL,
+  `p_email` varchar(100) NOT NULL,
+  `p_phoneNum` varchar(15) DEFAULT NULL,
+  `p_address` varchar(100) DEFAULT NULL,
+  `p_postCode` varchar(10) DEFAULT NULL,
+  `p_country` varchar(50) DEFAULT NULL,
+  `p_state` varchar(50) DEFAULT NULL,
+  `p_department` varchar(100) DEFAULT NULL,
+  `p_bodyNumber` varchar(10) DEFAULT NULL,
+  `p_position` varchar(100) DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `summon`
+--
+
+CREATE TABLE `summon` (
+  `sum_id` int(11) NOT NULL,
+  `sum_date` date DEFAULT NULL,
+  `sum_vModel` varchar(50) NOT NULL,
+  `sum_vBrand` varchar(100) NOT NULL,
+  `sum_vPlate` varchar(10) NOT NULL,
+  `sum_location` varchar(10) NOT NULL,
+  `sum_violationType` varchar(200) NOT NULL,
+  `sum_demerit` int(11) NOT NULL,
+  `sum_status` varchar(200) DEFAULT NULL,
+  `sum_QR` varchar(200) DEFAULT NULL,
+  `vt_id` int(11) DEFAULT NULL,
+  `v_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `u_id` int(11) NOT NULL,
+  `u_email` varchar(100) NOT NULL,
+  `u_password` varchar(255) NOT NULL,
+  `u_type` enum('Unit Keselamatan Staff','Administrators','Student') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle`
+--
+
+CREATE TABLE `vehicle` (
+  `v_id` int(11) NOT NULL,
+  `v_vehicleType` enum('MOTORCYCLE','CAR') NOT NULL,
+  `v_brand` varchar(50) NOT NULL,
+  `v_roadTaxValidDate` date NOT NULL,
+  `v_licenceValidDate` date NOT NULL,
+  `v_licenceClass` varchar(5) NOT NULL,
+  `v_phoneNum` int(11) NOT NULL,
+  `v_vehicleGrant` blob NOT NULL,
+  `v_approvalStatus` enum('Reject','Approve') DEFAULT NULL,
+  `v_remarks` text DEFAULT NULL,
+  `v_qrCode` varchar(500) DEFAULT NULL,
+  `v_plateNum` varchar(10) NOT NULL,
+  `v_model` varchar(50) NOT NULL,
+  `u_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `violationtype`
+--
+
+CREATE TABLE `violationtype` (
+  `vt_id` int(11) NOT NULL,
+  `vt_name` varchar(200) NOT NULL,
+  `vt_demeritPoints` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookinfo`
+--
+ALTER TABLE `bookinfo`
+  ADD PRIMARY KEY (`b_id`),
+  ADD KEY `u_id` (`u_id`),
+  ADD KEY `v_id` (`v_id`),
+  ADD KEY `ps_id` (`ps_id`);
+
+--
+-- Indexes for table `parkspace`
+--
+ALTER TABLE `parkspace`
+  ADD PRIMARY KEY (`ps_id`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`p_id`),
+  ADD KEY `u_id` (`u_id`);
+
+--
+-- Indexes for table `summon`
+--
+ALTER TABLE `summon`
+  ADD PRIMARY KEY (`sum_id`),
+  ADD KEY `vt_id` (`vt_id`),
+  ADD KEY `v_id` (`v_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`u_id`),
+  ADD UNIQUE KEY `u_email` (`u_email`);
+
+--
+-- Indexes for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`v_id`),
+  ADD KEY `u_id` (`u_id`);
+
+--
+-- Indexes for table `violationtype`
+--
+ALTER TABLE `violationtype`
+  ADD PRIMARY KEY (`vt_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookinfo`
+--
+ALTER TABLE `bookinfo`
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `summon`
+--
+ALTER TABLE `summon`
+  MODIFY `sum_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  MODIFY `v_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `violationtype`
+--
+ALTER TABLE `violationtype`
+  MODIFY `vt_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookinfo`
+--
+ALTER TABLE `bookinfo`
+  ADD CONSTRAINT `bookinfo_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`),
+  ADD CONSTRAINT `bookinfo_ibfk_2` FOREIGN KEY (`v_id`) REFERENCES `vehicle` (`v_id`),
+  ADD CONSTRAINT `bookinfo_ibfk_3` FOREIGN KEY (`ps_id`) REFERENCES `parkspace` (`ps_id`);
+
+--
+-- Constraints for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
+
+--
+-- Constraints for table `summon`
+--
+ALTER TABLE `summon`
+  ADD CONSTRAINT `summon_ibfk_1` FOREIGN KEY (`vt_id`) REFERENCES `violationtype` (`vt_id`),
+  ADD CONSTRAINT `summon_ibfk_2` FOREIGN KEY (`v_id`) REFERENCES `vehicle` (`v_id`);
+
+--
+-- Constraints for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD CONSTRAINT `vehicle_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
